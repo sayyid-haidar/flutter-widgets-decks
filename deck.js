e389db1a81b3ae82607277aa37379a6762bddd5e
sayid_haidar <sayyid.abdul.aziz.haidar@gmail.com>
2020-09-14 04:06:24 +0700

    start: flutter-widget-decks

--- deck.js
+++ deck.js
@@ -3,9 +3,9 @@ import "./style.css";
 
 // import `slides` from your mdx files and spread into `slides` export
 import { slides as introduction } from "./slides/00-introduction.mdx";
-import { slides as webDevelopment } from "./slides/01-web-development.mdx";
-import { slides as whatsIsHtml } from "./slides/02-whats-is-html.mdx";
-import { slides as tagAndContent } from "./slides/03-tag-and-content.mdx";
+import { slides as introductionFlutter } from "./slides/01-introduction-to-flutter.mdx";
+import { slides as whatIsFlutterWidget } from "./slides/02-whats-is-flutter-widget.mdx";
+import { slides as basicWidgets } from "./slides/03-basic-widgets.mdx";
 import { slides as attributeAndValue } from "./slides/04-attribute-and-value.mdx";
 import { slides as htmlInWebsite } from "./slides/05-html-in-web.mdx";
 import { slides as directory } from "./slides/06-directory.mdx";
@@ -17,9 +17,9 @@ export { dracula as theme } from "code-surfer";
 // Spread your slides here...
 export const slides = [
   ...introduction,
-  ...webDevelopment,
-  ...whatsIsHtml,
-  ...tagAndContent,
+  ...introductionFlutter,
+  ...whatIsFlutterWidget,
+  ...basicWidgets,
   ...attributeAndValue,
   ...htmlInWebsite,
   ...directory,
